37bddc157eb1c197298b49605259333c2cb2adec
Ricardo Bustamante <ricardo.bustamante@tapps.com.br>
2020-02-01 13:03:04 -0300

    Add title screen and TMP

---
+++ Assets/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class LoadSceneButton : MonoBehaviour
+{
+    public string SceneName;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(SceneName));
+    }
+}
---
+++ Assets/Scripts/UI/ToggleModalButton.cs
@@ -0,0 +1,16 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ToggleModalButton : MonoBehaviour
+{
+    [Required] public Canvas ModalCanvas;
+
+    private void Start()
+    {
+        var modal = ModalCanvas.gameObject;
+        modal.SetActive(false);
+        GetComponent<Button>().onClick.AddListener(() => modal.SetActive(!modal.activeSelf));
+    }
+}
