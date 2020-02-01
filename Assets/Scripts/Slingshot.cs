7656c95ac9002dd1978cdcc14f451ef656d060ba
Hyago Oliveira <hyago.oliveira@tapps.com.br>
2020-01-31 22:41:31 -0300

    Slingshot scene

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public sealed class Slingshot : MonoBehaviour 
+{
+    public float maxTensionDistance = 1f;
+
+	private void Start () 
+	{
+		
+	}
+
+	private void Update () 
+	{
+		
+	}
+	
+}
