96ffb7c266f442e5050a13c2f97a4fd223569478
Ricardo Bustamante <ricardo.bustamante@tapps.com.br>
2020-02-01 15:08:38 -0300

    Separate sandbox scenes.

---
+++ Assets/Scripts/UI/Opening.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Opening : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOVirtual.DelayedCall(0.5f, () => { SceneManager.LoadScene("SlingshotScene"); });
+    }
+}
