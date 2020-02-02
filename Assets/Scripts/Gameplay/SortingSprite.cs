125055834e7a6b876b3013ca0c41739738968d18
Hyago Oliveira <hyago.oliveira@tapps.com.br>
2020-02-02 15:29:15 -0300

    no message

---
+++ Assets/Scripts/Gameplay/SortingSprite.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(SpriteRenderer))]
+public sealed class SortingSprite : MonoBehaviour 
+{
+    [SerializeField] private ScoreCalculator calculator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    public Sprite[] sprites;
+
+
+    private void Reset()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Awake() 
+	{
+        Sort();
+    }
+
+    public void Sort()
+    {
+        if (sprites.Length == 0) return;
+
+        int index = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[index];
+        calculator.MaskSprite = sprites[index];
+    }
+}
