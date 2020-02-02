5ac81f7a68377d5fa661b15eca6af9a4dd84aeea
Hyago Oliveira <hyago.oliveira@tapps.com.br>
2020-02-02 12:13:31 -0300

    correção audios

---
+++ Assets/Scripts/Gameplay/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace nameSpaceName
+{
+	[CreateAssetMenu(fileName = "NewGameSettings", menuName = "Scriptable Objects Menu", order = 101)]
+	public sealed class GameSettings : ScriptableObject
+	{
+		public int data = 1; 
+	}
+}
