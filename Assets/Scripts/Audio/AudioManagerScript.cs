b187733a3667549eaef13aaca46d2e16ffbb2e08
mauriciojsantos <mauricio.juliano@tapps.com.br>
2020-02-02 12:25:00 -0300

    Made a pitch Shifter for the rubberband

---
+++ Assets/Scripts/Audio/AudioManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.onValueChanged.AddListener(SetLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("RubberPitch", sliderValue);
+    }
+}
